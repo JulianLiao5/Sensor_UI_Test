3002ae3253c704c7079e2d250c8b756a58ff70e4
JulianLiao <julianliao5@gmail.com>
2021-01-09 22:08:11 +0800

    remove wrong old transter python tools.
     Changes to be committed:
    	modified:   tools/transfer_git_1.py
    	deleted:    tools/transfer_git_1_20190903.py

--- tools/transfer_git_1.py
+++ tools/transfer_git_1.py
@@ -18,9 +18,7 @@ def loadradar(filename):
     with open('transfer.txt', 'w+') as filter_file:
             for line in myfile.readlines():
                 line = line[:-1]    # deletes extra line
-                if re.search("buff", line, re.IGNORECASE):
-                    np.savetxt(filter_file, np.array(line).reshape(1), fmt="%s")
-                elif re.search("Angle", line, re.IGNORECASE):
+                if re.search("Angle", line, re.IGNORECASE):
                     angle_name, angle_var = line.partition("Angle: ")[::2]
                     angle_item = angle_var.split(" ")[0]
                     angle = float(angle_item)
@@ -28,7 +26,7 @@ def loadradar(filename):
                     distance_item = distance_var.split(" ")[0]
                     distance = float(distance_item)
                     distance_x = distance * math.cos(angle * DEG2RAD)
-                    distance_y = distance * math.sin(angle * DEG2RAD)
+                    distance_y = -(distance * math.sin(angle * DEG2RAD))
                     new_line = distance_name + "distance_x: " + "{0:.4f}".format(distance_x) + " distance_y: " + "{0:.4f}".format(distance_y) + " distance: " + distance_var
                     np.savetxt(filter_file, np.array(new_line).reshape(1), fmt="%s")
 
